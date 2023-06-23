7bab440ccfac2333f45acfa1a337e8cadde11246
imranprice96 <imranprice96@gmail.com>
2023-06-23 16:48:19 +1200

    Adding files and changes required for deployment

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ dotenv.config();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.qysvlcw.mongodb.net/?retryWrites=true&w=majority`;
+const dev_db_url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster1.qysvlcw.mongodb.net/?retryWrites=true&w=majority`;
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
